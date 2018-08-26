7c64a3ceb4d471e2b1a6074f280846e4af7a6d4b
Neil.Sarkar <ansarkar@outlook.com>
2018-08-26 18:01:30 +1000

    did client side init

---
+++ client/js/App.js
@@ -0,0 +1,17 @@
+import  React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render(){
+    return (
+      <div className="App">
+        hello world
+      </div>
+    );
+  }
+}
+
+export default App;
